077a162a3515226a84a9d01990fbb535a7d68bf6
Saksham Malik <113609991+Saksham2874@users.noreply.github.com>
2024-04-23 23:29:02 +0530

    Add files via upload

---
+++ subsequence_subsets_of_a_string.java
@@ -0,0 +1,14 @@
+public class subsequence_subsets_of_a_string {
+    public static void main(String[] args) {
+        subseq("","abcde");
+    }
+    static void subseq(String p , String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subseq(p+ch,up.substring(1));
+        subseq(p,up.substring(1));
+    }
+}
