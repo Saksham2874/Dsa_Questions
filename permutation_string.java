6f9722856640a9f40a3c3ea004bdd12c6eaa0c8f
Saksham Malik <113609991+Saksham2874@users.noreply.github.com>
2024-04-26 17:02:19 +0530

    Add files via upload

--- permutation_string.java
+++ permutation_string.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+
 public class permutation_string {
     public static void main(String[] args) {
         permutation("","abc");
+        ArrayList<String> ans1 = new ArrayList<>();
+        ans1 = permutationArrayList("","abc");
+        System.out.println(ans1);
     }
     static void permutation(String p , String up){
         if(up.isEmpty()){
@@ -16,4 +21,22 @@ public class permutation_string {
         }
 
     }
+    static ArrayList<String> permutationArrayList(String p , String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> ans = new ArrayList<>();
+        for (int i = 0; i <= p.length() ; i++) {
+            String f = p.substring(0,i);
+            String s = p.substring(i,p.length());
+            ans.addAll(permutationArrayList(f+ch+s,up.substring(1)));
+
+        }
+        return ans;
+
+    }
+
 }
