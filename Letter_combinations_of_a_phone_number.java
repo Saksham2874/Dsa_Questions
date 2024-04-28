5445bac4d3c431accf706b192c4847f1da382596
Saksham Malik <113609991+Saksham2874@users.noreply.github.com>
2024-04-28 14:07:14 +0530

    Add files via upload

---
+++ Letter_combinations_of_a_phone_number.java
@@ -0,0 +1,19 @@
+public class Letter_combinations_of_a_phone_number {
+    public static void main(String[] args) {
+        pad("","19");
+    }
+    static void pad(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        int digit = up.charAt(0) - '0';
+        
+        for (int i = ((digit-1)*3); i <(digit*3) ; i++) {
+            char ch = (char)('a' + i);
+            pad(p+ch,up.substring(1));
+
+        }
+    }
+}
