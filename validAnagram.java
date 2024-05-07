c0de62b38f02aca241b616f6768a15b95d8ad732
Saksham Malik <113609991+Saksham2874@users.noreply.github.com>
2024-05-07 20:06:31 +0530

    Add files via upload

---
+++ validAnagram.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class validAnagram {
+    public static void main(String[] args) {
+       String s = "anagram";
+       String t = "nagaram";
+        System.out.println(valid(s,t));
+    }
+    public static boolean valid(String s , String t){
+        if(s.length() != t.length()) return false;
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        for (int i = 0; i < arr1.length; i++) {
+            if(arr1[i] != arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
