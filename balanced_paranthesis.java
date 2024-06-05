73d65d39266766322031e7a03acd109f9b81e9ac
Saksham Malik <113609991+Saksham2874@users.noreply.github.com>
2024-06-06 01:08:05 +0530

    Add files via upload

---
+++ balanced_paranthesis.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class balanced_paranthesis {
+    public static boolean isBalanced(String str){
+        Stack<Character> st = new Stack<>();
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if(ch == '('){
+                st.push(ch);
+            }
+            else{
+                if(st.size() == 0) return false;
+                if(st.peek() == '(') st.pop();
+            }
+        }
+        if(st.size() > 0) return false;
+        else return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(isBalanced(str));
+    }
+}
